8f0642cd06d5da95be2ca1b1e5a7fb0e51e0af7e
abhay8046 <abhaydere8046@gmail.com>
2023-06-08 18:06:19 -0500

    https://github.com/abhay8046/SuggestAI.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
+const API_KEY = process.env.REACT_APP_API_KEY;; // Replace with your actual OpenAI API key
+const API_ENDPOINT = 'https://api.openai.com/v1/completions';
+
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [showApp, setShowApp] = useState(false);
+
+  const handleInputChange = (e) => {
+    const text = e.target.value;
+    setInputText(text);
+    getSuggestions(text);
+  };
+
+  const getSuggestions = async (text) => {
+    if (text.trim() === '') {
+      setSuggestions([]);
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        API_ENDPOINT,
+        {
+          model: "text-davinci-003",
+          prompt: text,
+          max_tokens: 50,
+          temperature: 0.7,
+          n: 3
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${API_KEY}`
+          }
+        }
+      );
+
+      const { choices } = response.data;
+      setSuggestions(choices.map(choice => choice.text));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleTryNowClick = () => {
+    setShowApp(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {!showApp && (
+        <div className="landing-screen">
+          <h2>Welcome to</h2>
+          <h1>SuggestAi</h1>
+          <p>Start typing in the text area below to experience AI-generated suggestions as you type.</p>
+          <button className="try-now-button" onClick={handleTryNowClick}>
+            Try Now
+          </button>
+        </div>
+      )}
+      {showApp && (
+        <div className="app">
+          <textarea
+            placeholder="Start typing to experience AI magic..."
+            value={inputText}
+            onChange={handleInputChange}
+          />
+          <div id="suggestions">
+            {suggestions.map((suggestion, index) => (
+              <p key={index}>{suggestion}</p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
